e4fb12029b6908a3a5ab1407d6a27afb1b391b28
Marcos Gomez <marcosgomezsanchez@gmail.com>
2019-11-05 00:57:03 +0000

    react-build

---
+++ client/build/precache-manifest.a788fb8c392d56d8c5943a82d0e79325.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a51372cf04a5703f08021d0e62b3120",
+    "url": "/index.html"
+  },
+  {
+    "revision": "53c5f536abd22a86380e",
+    "url": "/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "ec4327a8180d5d4115de",
+    "url": "/static/css/main.57ed44bd.chunk.css"
+  },
+  {
+    "revision": "53c5f536abd22a86380e",
+    "url": "/static/js/2.bf9c0291.chunk.js"
+  },
+  {
+    "revision": "ec4327a8180d5d4115de",
+    "url": "/static/js/main.8e65a7b6.chunk.js"
+  },
+  {
+    "revision": "5178fb100f9f6a9821ca",
+    "url": "/static/js/runtime-main.2877809b.js"
+  }
+]);
